854dbadc1093f1f81378560b06a110e40d20886d
Ivan-Bliminse <arturgorbunov3@gmail.com>
2022-09-19 18:36:52 +0300

    test props to card component

--- src/Components/Main/Card/Card.js
+++ src/Components/Main/Card/Card.js
@@ -3,15 +3,12 @@ import styles from "./Card.module.css";
 
 export default class Card extends Component {
   render() {
+    const { url, name, price } = this.props;
     return (
       <div className={styles.card}>
-        <img
-          src="http://unsplash.it/356/338"
-          alt="pic"
-          className={styles.card__image}
-        />
-        <div>Name</div>
-        <div>Price</div>
+        <img src={url} alt="pic" className={styles.card__image} />
+        <div>{name}</div>
+        <div>{price}</div>
       </div>
     );
   }
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,12 +1,27 @@
 import React, { Component } from "react";
 import Card from "./Card/Card";
+import styles from "./Main.module.css";
 
 export default class Main extends Component {
   render() {
+    const testProps = [
+      { url: "http://unsplash.it/355/338", name: "Alpha", Price: "30.00" },
+      { url: "http://unsplash.it/356/338", name: "Beta", Price: "42.00" },
+      { url: "http://unsplash.it/357/338", name: "Charlie", Price: "13.00" },
+    ];
     return (
       <div>
         <h2>Category name</h2>
-        <Card />
+        <div className={styles.products}>
+          {testProps.map((card) => (
+            <Card
+              key={card.name}
+              url={card.url}
+              name={card.name}
+              price={card.Price}
+            />
+          ))}
+        </div>
       </div>
     );
   }
