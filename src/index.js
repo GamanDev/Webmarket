8d5cdf78827cbc8228fc3953c7c08c7848884a5a
Ivan-Bliminse <arturgorbunov3@gmail.com>
2022-09-21 18:05:14 +0300

    redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { Provider } from "react-redux";
+import { store } from "./Redux/store";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000",
@@ -12,6 +14,8 @@ const client = new ApolloClient({
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ApolloProvider client={client}>
-    <App client={client} />
+    <Provider store={store}>
+      <App client={client} />
+    </Provider>
   </ApolloProvider>
 );
